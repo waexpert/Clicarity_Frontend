905de6c1b96935229b1a1b305573dd13957ad01f
waexpert <contact@wa.expert>
2025-07-09 13:39:55 +0530

    Small Change in apiConfig

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 // All The Api Routes
 
 export const getAllRecords = `${import.meta.env.VITE_APP_BASE_URL}/data/getAllData`;
-export const updateRecord = `${import.meta.env.VITE_APP_BASE_URL}/data/updateMultiple?`;
+export const updateRecord = `${import.meta.env.VITE_APP_BASE_URL}/data/updateMultiple`;
 export const createRecord = `${import.meta.env.VITE_APP_BASE_URL}/data/createRecord`
 
 

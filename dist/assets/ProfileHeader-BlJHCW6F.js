e4cd1479dce6652a6777ef53dccf5cbd1194b252
1lakshy <lakshy18sharma@gmail.com>
2026-01-20 19:42:45 +0530

    Build v1.74 -> Moved App.txs,StatusUpdate,Show process as per Roles and Custom table to Performance optimsed Logic using hooks

---
+++ dist/assets/ProfileHeader-BlJHCW6F.js
@@ -0,0 +1,1 @@
+import{u as e,j as r}from"./index-DhckfCQI.js";const t=()=>(e(),r.jsx("div",{}));export{t as P};
---
+++ dist/assets/apiConfig-CtLEPF4c.js
@@ -0,0 +1,1 @@
+const t="https://click.wa.expert/api/data/getAllData",a="https://click.wa.expert/api/data/createRecord",e="https://click.wa.expert/api/data/getRecordById";export{e as a,a as c,t as g};

b6c34d0888e55773386cceb9a902877e16eb430c
1lakshy <lakshy18sharma@gmail.com>
2026-01-28 17:31:56 +0530

    Build v1.78 -> Added the New Main and Split Job form

--- dist/assets/ProfileHeader-Bwd92inC.js
+++
@@ -1,1 +0,0 @@
-import{u as e,j as r}from"./index-B9v0YaNK.js";const t=()=>(e(),r.jsx("div",{}));export{t as P};
--- dist/assets/apiConfig-CtLEPF4c.js
+++
@@ -1,1 +0,0 @@
-const t="https://click.wa.expert/api/data/getAllData",a="https://click.wa.expert/api/data/createRecord",e="https://click.wa.expert/api/data/getRecordById";export{e as a,a as c,t as g};
